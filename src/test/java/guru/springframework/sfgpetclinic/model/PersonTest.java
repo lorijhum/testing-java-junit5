08928f782fd8366cd21d3334b8f9703e84f3ed07
Lori Humphries <lhemailme@aim.com>
2019-05-25 09:11:10 -0400

    adding repeating tests

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -8,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class PersonTest implements ModelTests {
+
     @Test
     void groupedAssertions() {
         //given
@@ -20,4 +23,16 @@ class PersonTest implements ModelTests {
 
 
     }
-}
+    @RepeatedTest(value=10, name= "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("my repeated test")
+    @Test
+
+    void repeatedTest() {
+       //to do write test we want to repeat.  display name gets put in repeatedTest display name,
+        //currentRepetition is put in that placeholder and totalRepetition is put in that placeholder
+        //so we get output of my repeated test: 1-10 
+        }
+
+
+
+    }
