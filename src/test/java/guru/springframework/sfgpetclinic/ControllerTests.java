a3abbbbedc7ca443cacd83f9cabfb62c700f5c17
Lori Humphries <lhemailme@aim.com>
2019-05-25 08:33:33 -0400

    adding default method to interface

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,7 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+@TestInstance(PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
+
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("This will run before all controller tests");
+    }
 }
