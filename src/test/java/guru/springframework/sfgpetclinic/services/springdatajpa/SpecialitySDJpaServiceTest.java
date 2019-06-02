40a475b2f773bac92524c5e0b6e2e6e9e853b97d
Lori Humphries <lhemailme@aim.com>
2019-06-02 09:38:44 -0400

    convert one test to bdd test

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -25,7 +25,7 @@ class SpecialitySDJpaServiceTest {
     SpecialitySDJpaService service;
 
     @Test
-    void findByIdBdd() {
+    void findById() {
         //given
         Speciality speciality = new Speciality();
         given(specialtyRepository.findById(1L)).willReturn(Optional.of(speciality));
@@ -42,72 +42,76 @@ class SpecialitySDJpaServiceTest {
 
     }
 
-    @Test
-    void findById() {
-        //here we create an instance of Speciality
-        Speciality speciality = new Speciality();
-        // then we say when calling findById(1L), we return the speciality we just created above
-       when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
-
-        //here we find the speciality by id
-        Speciality foundSpeciality = service.findById(1L);
-
-
-        assertThat(foundSpeciality).isNotNull();
-
-        //here we verify that the method was called once (that is the default of verify, once)
-        verify(specialtyRepository).findById(1L);
-
-        //here we verify that the method was called to find any id of type long
-        verify(specialtyRepository).findById(anyLong());
-    }
 
     @Test
     void deleteByObject() {
+        //given
         Speciality speciality = new Speciality();
+        //when
         service.delete(speciality);
 
         //using an argument matcher in the verify to check that we deleted an object of the Speciality Class
-        verify(specialtyRepository).delete(any(Speciality.class));
+        //then
+
+        then(specialtyRepository).should().delete(any(Speciality.class));
     }
     @Test
     void deleteById() {
+        //given none
+
+        //when
         service.deleteById(1L);
         service.deleteById(1L);
         // the default for the verify below is 1, so without ,times, it verifies the method was called once
-        verify(specialtyRepository,times(2)).deleteById(1L);
+        //then
+        then(specialtyRepository).should(times(2)).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtLeast() {
+        //given
+
+        //when
         service.deleteById(1L);
         service.deleteById(1L);
+
+        //then
         //checks that the method was called at least one time
-        verify(specialtyRepository, atLeastOnce()).deleteById(1L);
+        then(specialtyRepository).should(atLeastOnce()).deleteById(1L);
     }
 
     @Test
     void deleteByIdAtMost() {
+        //when
         service.deleteById(1L);
         service.deleteById(1L);
         service.deleteById(1L);
+
+        //then
         //checks that the method was called at most 5 times
-        verify(specialtyRepository, atMost(5)).deleteById(1L);
+        then(specialtyRepository).should(atMost(5)).deleteById(1L);
     }
 
     @Test
     void deleteByIdNever() {
+        //when
         service.deleteById(1L);
         service.deleteById(1L);
         service.deleteById(1L);
+
+        //then
         //checks that the method was called at most 5 times
-        verify(specialtyRepository, atMost(5)).deleteById(1L);
+        then(specialtyRepository).should(atMost(5)).deleteById(1L);
         //checks that it was never called for id 2
-        verify(specialtyRepository, never()).deleteById(2L);
+        then(specialtyRepository).should(never()).deleteById(2L);
     }
 
     @Test
     void delete() {
+        //when
         service.delete(new Speciality());
+        //then
+        then(specialtyRepository).should().delete(any(Speciality.class));
+
     }
 }
