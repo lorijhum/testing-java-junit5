4a5e1056a792d87862eaa2ea009b95dd2b6afc93
Lori Humphries <lhemailme@aim.com>
2019-06-08 08:53:00 -0400

    adding controller test

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,24 +10,54 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
-
+    private static final String REDIRECT_OWNERS_5 = "redirect:/owners/5";
+    private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    @Mock
+    private BindingResult bindingResult;
 
     @Mock
-    Owner owner;
-
-    @Mock
-    BindingResult bindingResult;
+    private OwnerService ownerService;
 
     @InjectMocks
-    OwnerController controller;
+    private OwnerController controller;
 
     @Test
     @DisplayName("Test correct view name is returned from controller")
     void processCreationFormWithoutErrors() {
 
+        //given
+        Owner owner = new Owner(5L, "Lucy", "Lou");
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(ownerService.save(any())).willReturn(owner);
+
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(REDIRECT_OWNERS_5);
+
+
+    }
+
+    @Test
+    void processCreationFormWithErrors() {
+        //given
+        Owner owner = new Owner(1L, "Lucy", "Lou");
+        given(bindingResult.hasErrors()).willReturn(true);
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(OWNERS_CREATE_OR_UPDATE_OWNER_FORM);
 
 
     }
